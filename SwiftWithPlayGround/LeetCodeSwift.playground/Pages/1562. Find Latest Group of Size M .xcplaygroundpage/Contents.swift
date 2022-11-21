4de468b7ebeae1dc4745ca1746243ff9e609ee20
Spark <50857240+sparkdoaz@users.noreply.github.com>
2022-11-21 15:28:16 +0800

    change name

---
+++ SwiftWithPlayGround/LeetCodeSwift.playground/Pages/1562. Find Latest Group of Size M .xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+import XCTest
+
+class Solution {
+    func findLatestStep(_ arr: [Int], _ m: Int) -> Int {
+        
+        return 0
+    }
+}
+
+class Tests: XCTestCase {
+    var solution = Solution()
+    
+    func test1() {
+        
+    }
+    
+    func test2() {
+        
+    }
+    
+    func test3() {
+        
+    }
+    
+    func test4() {
+        
+    }
+}
+
+Tests.defaultTestSuite.run()
---
+++ SwiftWithPlayGround/LeetCodeSwift.playground/Pages/2420. Find All Good Indices.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+// https://leetcode.com/problems/find-all-good-indices/
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
